7938b9b7a044cac7afafae22948f05c67c089fe3
fishstar2023 <urania@UraniadeMacBook-Air.local>
2025-10-17 14:02:51 +0800

    fix: front-end CORS problem

--- app/main.py
+++ app/main.py
@@ -30,9 +30,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5500",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
-        "http://localhost:5500",
         "http://127.0.0.1:5500",
         "null"  # 允許本地文件訪問
     ],
